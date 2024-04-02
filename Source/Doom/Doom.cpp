8ab021b30b19c253b56af6c6180f5c8809f29b93
seafom <sibelle.bergier@artfx.fr>
2024-04-02 12:42:56 +0200

    [add] movement

---
+++ Source/Doom/Doom.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Doom.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Doom, "Doom" );
---
+++ Source/Doom/DoomGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOOM_API ADoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
